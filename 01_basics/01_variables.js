4f018ff26d1323508ec4c72e426918a04ac3866a
Shubham0247 <shubhamaudattapure47@gmail.com>
2023-12-11 22:58:08 +0530

    let, var and const variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 144553
+let accountEmail = "shubham@gmail.com"
+var accoundPassword = "12345"
+accountCity = "Jaipur"
+
+let accountState;
+
+// accountId = 2
+
+accountEmail = "sa@c.com"
+accoundPassword = "3212313"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accoundPassword,accountCity])
+
+console.log(accountState);
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+
+*/
---
+++ 01_basics/tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "shubham@gmail.com"
+var accoundPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+
+accountEmail = "sa@c.com"
+accoundPassword = "3212313"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accoundPassword,accountCity])
