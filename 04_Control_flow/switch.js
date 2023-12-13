c94c3d446f1c8eada1e60353f465e637e30e556f
Shubham0247 <shubhamaudattapure47@gmail.com>
2023-12-13 17:52:06 +0530

    switch case in javascript

---
+++ 04_Control_flow/switch.js
@@ -0,0 +1,67 @@
+// syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = "april"
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+//     case 6:
+//         console.log("June");
+//         break;
+//     case 7:
+//         console.log("July");
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "april"
+
+switch (month) {
+    case "January":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+    case "may":
+        console.log("May");
+        break;
+    case "june":
+        console.log("June");
+        break;
+    case "july":
+        console.log("July");
+        break;
+
+    default:
+        break;
+}
