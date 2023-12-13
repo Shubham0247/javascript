f52db9c26f9ff0426281124cc82434ddd9c3e71d
Shubham0247 <shubhamaudattapure47@gmail.com>
2023-12-13 23:30:46 +0530

    for loops in javascript

---
+++ 05_iterations/01_forLoop.js
@@ -0,0 +1,40 @@
+//syntax
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for(let i = 0; i<=10; i++) {
+//     const element = i;
+//     if(element == 5){
+//         console.log("5 is best number");
+//     }
+//     // console.log(element);
+// }
+
+// for (let i = 0; i <=10; i++) {
+//     console.log(`Outer loop current value:- ${i}`);
+//     for (let j = 0; j <= 5; j++) {
+//         console.log(`\tInner loop current value:- ${j}`); 
+//     }
+    
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Multiplication table of ${i} is :- `);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} x ${j} = ${i*j}`);
+//     }
+// }
+
+let myArray = ["flash","batman","superman"]
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+}
+
+// break and continue
+
+
