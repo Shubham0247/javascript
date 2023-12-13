464c45af903de0c2538514871e94b106c8be25e6
Shubham0247 <shubhamaudattapure47@gmail.com>
2023-12-13 23:31:01 +0530

    while loop in javascript

---
+++ 05_iterations/02_whileLoop.js
@@ -0,0 +1,22 @@
+
+// let index = 1;
+// while (index <= 10) {
+//     console.log(index);
+//     index+=2;
+// }
+
+let myArray = ["flash", "batman", "superman"]
+let arr = 0;
+while(arr < myArray.length) {
+    console.log(myArray[arr++]);
+}
+
+// do while
+
+// let score = 1
+let score = 11
+
+do {
+    console.log("Score is "+score);
+    score++;
+} while(score <= 10)
