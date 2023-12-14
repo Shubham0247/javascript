36ab427d562352b8e15dece6d3ecf1d4125c24b2
Shubham0247 <shubhamaudattapure47@gmail.com>
2023-12-14 20:55:08 +0530

    Number guessing game

---
+++ 07_Projects/04_GuessANumber/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+const targetNumber = Math.floor(Math.random()*100 + 1)
+// console.log(targetNumber);
+
+const form = document.querySelector('form');
+
+const userInput = form.guess;
+
+console.log(userInput);
+
