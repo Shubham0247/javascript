e8ef3e94e5ec9a7b2b95c274cb2877bce9b45efb
Shubham0247 <shubhamaudattapure47@gmail.com>
2023-12-16 19:23:53 +0530

    static methods in js

---
+++ 10_classes_and_oop/static.js
@@ -0,0 +1,29 @@
+class User { 
+    constructor(username) {
+        this.username = username
+    }
+
+    logMe() {
+        console.log(`Username: ${this.username}`)
+    }
+
+    static createId() {
+        return `123`
+    }
+}
+
+const Shubham = new User("Shubham")
+// console.log(Shubham.createId()); // this will not work because createId() is static and we cannot call static variables or methods using objects
+
+console.log(User.createId());
+
+class Teacher extends User {
+    constructor(username,email) {
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@gmail.com")
+// console.log(iphone.createId()); // will not work because of static
+
